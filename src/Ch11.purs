eace9ee3e3a667929c6c62b9cc46852051a46254
Hans-J. Schmid <knock@myopendoor.de>
2021-12-23 17:58:33 +0100

    finishes Ch. 11.

--- src/Ch11.purs
+++ src/Ch11.purs
@@ -1,12 +1,13 @@
 module Ch11 where
 
-import Prelude (Unit, show, type (~>), ($), flip, discard, negate, otherwise)
-import Data.List (List(..), (:))
-import Data.Foldable (class Foldable, foldl)
-import Data.List.Types (NonEmptyList(..))
-import Data.Maybe
+import Prelude (Unit, show, type (~>), ($), flip, discard, negate, otherwise, (+), (<>), (<<<))
+import Data.List (List(..), (:), singleton, foldl, foldr, foldMap)
 import Data.NonEmpty (NonEmpty, (:|))
+import Data.Foldable (class Foldable)
+import Data.List.Types (NonEmptyList(..))
+import Data.Maybe (Maybe(..))
 import Data.Ord (class Ord, (>=))
+import Data.Semiring
 import Effect (Effect)
 import Effect.Console (log)
 
@@ -24,7 +25,38 @@ findMax (a : as) = Just $ foldl max a as
 findMaxNE :: ∀ a. Ord a => NonEmptyList a  -> a
 findMaxNE (NonEmptyList (a :| as)) = foldl max a as
 
--- foldl1 :: ∀ f a. Foldable f => (a -> a -> a) -> NonEmpty f a -> a
+findMaxNE1 :: ∀ a. Ord a => NonEmptyList a -> a
+findMaxNE1 (NonEmptyList ne) = foldl1 max ne
+
+foldl1 :: ∀ f a. Foldable f => (a -> a -> a) -> NonEmpty f a -> a
+-- foldl1 fun (NonEmpty a fa) = foldl fun a fa
+foldl1 fun (a :| fa) = foldl fun a fa
+
+sum :: List Int -> Int
+sum Nil      = 0
+sum (i : is) = i + sum is
+
+sum1 :: List Int -> Int
+sum1 = go 0 where
+  go acc Nil      = acc
+  go acc (i : is) = go (i + acc) is
+
+sum2 :: List Int -> Int
+sum2 = foldl (+) 0
+
+sum3 :: ∀ a f. Semiring a => Foldable f => f a -> a
+sum3 = foldl (+) zero
+
+data Tree a = Leaf a | Node (Tree a) (Tree a)
+
+toList :: ∀ a. Tree a -> List a
+toList (Leaf leaf)       = singleton leaf
+toList (Node left right) = toList left <> toList right
+
+instance foldableTree :: Foldable Tree where
+  foldl fn acc = foldl fn acc <<< toList
+  foldr fn acc = foldr fn acc <<< toList
+  foldMap fn   = foldMap fn <<< toList
 
 test :: Effect Unit
 test = do
@@ -35,5 +67,16 @@ test = do
   -- log $ show $ findMax "" ("a" : "bbb" : "c" : Nil)
   log $ show $ findMax (37 : 311 : -1 : 2 : 84 : Nil)
   log $ show $ findMax ("a" : "bbb" : "c" : Nil)
-  log $ show $ findMaxNE (NonEmptyList $ 37 :| (311 : -1 : 2 : 84 : Nil))
-  log $ show $ findMaxNE (NonEmptyList $ "a" :| ("bbb" : "c" : Nil))
+  log $ show $ findMaxNE  (NonEmptyList $ 37 :| (311 : -1 : 2 : 84 : Nil))
+  log $ show $ findMaxNE1 (NonEmptyList $ 37 :| (311 : -1 : 2 : 84 : Nil))
+  log $ show $ findMaxNE  (NonEmptyList $ "a" :| ("bbb" : "c" : Nil))
+  log $ show $ findMaxNE1 (NonEmptyList $ "a" :| ("bbb" : "c" : Nil))
+  log $ show $ sum  (1 : 2 : 3 : Nil)
+  log $ show $ sum1 (1 : 2 : 3 : Nil)
+  log $ show $ sum2 (1 : 2 : 3 : Nil)
+  log $ show $ sum3 (1 : 2 : 3 : Nil)
+  log $ show $ sum3 (1.0 : 2.0 : 3.0 : Nil)
+  log $ show $ sum3 [1, 2, 3]
+  log $ show $ sum3 [1.0, 2.0, 3.0]
+  log $ show $ toList (Node (Node (Leaf 5) (Node (Leaf (-1)) (Leaf 14))) (Leaf 99)) -- (5 : -1 : 14 : 99 : Nil)
+  log $ show $ sum3   (Node (Node (Leaf 5) (Node (Leaf (-1)) (Leaf 14))) (Leaf 99)) -- 117
--- src/Main.purs
+++ src/Main.purs
@@ -2,11 +2,16 @@ module Main where
 
 import Prelude
 import Effect (Effect)
--- import Ch5 as Ch5
+-- import Ch5  as Ch5
 -- import Ch7a as Ch7a
 -- import Ch7b as Ch7b
--- import Ch9 as Ch9
+-- import Ch9  as Ch9
 import Ch11 as Ch11
 
 main :: Effect Unit
-main = Ch11.test
+main =
+  -- Ch5.test
+  -- Ch7a.test
+  -- Ch7b.test
+  -- Ch9.test
+  Ch11.test
